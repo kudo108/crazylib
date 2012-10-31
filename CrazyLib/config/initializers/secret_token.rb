0f26096e402877bead6f507ef58a90b41f0460a8
ahn.willta@gmail.com <ahn.willta@gmail.com@647d869f-c422-2f21-b4e5-c12cc13286ea>
2012-10-31 15:34:35 +0000

    Rails app skeleton

---
+++ CrazyLib/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrazyLib::Application.config.secret_token = '4ac67612d042b14949c4829ff55018ea7f556cc14c46025aa8dd6f1d5dfaa370fe7d11d46f463608dd820080074407db673fa0815dad847ed76255a389d1b035'
