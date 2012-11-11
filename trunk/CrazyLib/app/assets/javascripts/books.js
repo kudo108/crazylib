d6eff8d18a0c711655de69f9b8f5a3ca5c4da2ca
kimjiwoo741@gmail.com <kimjiwoo741@gmail.com@647d869f-c422-2f21-b4e5-c12cc13286ea>
2012-11-11 08:21:27 +0000

    version 1.1

---
+++ trunk/CrazyLib/app/assets/javascripts/books.js
@@ -0,0 +1,8 @@
+function moveCloud(){
+	var px = $("body").css("background-position-x");
+	px = parseInt(px.replace("px",""))+1+"px";
+	if(px == "5000px") px = "1px";
+	$("body").css("background-position-x",px);
+	setTimeout(moveCloud,100);
+}
+//setTimeout(moveCloud,100)
