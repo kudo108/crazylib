a83d34aa18c12010e8cd00c9d29e6cbf32877ae8
quan.nguyenvan000@gmail.com <quan.nguyenvan000@gmail.com@647d869f-c422-2f21-b4e5-c12cc13286ea>
2012-12-01 07:42:52 +0000

    Xem danh sách thông tin thành viên

--- CrazyLib/app/controllers/users_controller.rb
+++ CrazyLib/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  layout 'admin', :only =>[:index]
   def show
     @user = User.find(params[:id])
+  end
+  def index
+    @users = User.all
   end
 end
